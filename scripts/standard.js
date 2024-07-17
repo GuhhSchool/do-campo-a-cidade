7993fe8a21bac15fbe562350f409619b5a1ae9bd
Gustavo <107372982+GuhhSchool@users.noreply.github.com>
2024-07-16 23:10:53 -0300

    Update standard.js

--- scripts/standard.js
+++ scripts/standard.js
@@ -1,5 +1,6 @@
 // Voltando ao topo
 function backToTop(continuar) {
     if (continuar) document.documentElement.scrollTop = 0;
+    else if (window.location.pathname.split('/').pop() === 'history.html') window.open('./#', '_self');
     else window.open('../#', '_self');
 }
