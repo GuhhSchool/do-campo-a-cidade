7201749ea73d6fa46b543af35020fff784923f12
Gustavo <107372982+GuhhSchool@users.noreply.github.com>
2024-08-07 18:23:29 -0300

    Delete scripts directory

--- scripts/standard.js
+++
@@ -1,6 +0,0 @@
-// Voltando ao topo
-function backToTop(continuar) {
-    if (continuar) document.documentElement.scrollTop = 0;
-    else if (window.location.pathname.split('/').pop() === 'historia.html') window.open('./#', '_self');
-    else window.open('../#', '_self');
-}
